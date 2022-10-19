2e59829d35bafae4a3cbcdf3c12d7902354dd297
Donato Marrazzo <donato.marrazzo@gmail.com>
2022-10-19 12:34:12 +0200

    missing message fixed

--- kafka-consumer/src/main/java/com/redhat/demo/EventConsumer.java
+++ kafka-consumer/src/main/java/com/redhat/demo/EventConsumer.java
@@ -1,5 +1,6 @@
 package com.redhat.demo;
 
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -10,29 +11,21 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 @ApplicationScoped
 public class EventConsumer {
 
-    private TreeMap<Long, Boolean> check = new TreeMap<>();
-    private Long last = 0L;
+    private static TreeMap<Long, Boolean> check = new TreeMap<>();
+    private static Long last = 1L;
     
     @Incoming("event")
     public void consume(ConsumerRecord<Long, String> record) {
         Long key = record.key(); // Can be `null` if the incoming record has no key
-        String value = record.value(); // Can be `null` if the incoming record has no value
-        String topic = record.topic();
-        int partition = record.partition();
 
-        System.out.println(key);
-        // for (long i = last; i < key; i++) {
-        //     check.put(i, false);
-        // }
-        // check.put(key, true);
-        // System.out.println("key:" + key);
-        // SortedMap<Long, Boolean> headMap = check.headMap(last);
-        // for (Long entry : headMap.keySet()) {
-        //     if (check.get(entry)== true) {
-        //         check.remove(entry);
-        //     }
-        // }
-        // last=key;
-        // System.out.println("Missing messages: "+check.values().stream().filter(v -> v == false).count());
+        for (long i = last; i < key; i++) {
+            check.put(i, false);
+        }
+        if (key >= last) last=key+1L;
+
+        check.remove(key);
+
+        System.out.println("key:" + key);
+        System.out.println("Missing messages: "+check.size());
     }
 }
