56888540562a65db513497d6f76e27eff070d25f
Donato Marrazzo <donato.marrazzo@gmail.com>
2022-10-20 16:03:09 +0200

    grafana

---
+++ k8s/07-create-datasource.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export GRAFANA_ACCESS_TOKEN=`oc serviceaccounts get-token grafana-serviceaccount`
+
+echo "apiVersion: 1
+datasources:
+- name: Prometheus
+  type: prometheus
+  url: https://thanos-querier.openshift-monitoring.svc.cluster.local:9091
+  access: proxy
+  basicAuth: false
+  withCredentials: false
+  isDefault: true
+  jsonData:
+    timeInterval: 5s
+    tlsSkipVerify: true
+    httpHeaderName1: \"Authorization\"
+  secureJsonData:
+    httpHeaderValue1: \"Bearer ${GRAFANA_ACCESS_TOKEN}\"
+  editable: true" > datasource.yaml
+  
+  oc create configmap grafana-config -n my-kafka --from-file=datasource.yaml
+
+  rm datasource.yaml
