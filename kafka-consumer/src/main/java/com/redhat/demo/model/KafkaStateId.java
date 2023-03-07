c762eadbaad44302995860f5eb9d4859e626cf11
Donato Marrazzo <donato.marrazzo@gmail.com>
2023-03-07 15:37:19 +0100

    hash and equals

--- kafka-consumer/src/main/java/com/redhat/demo/model/KafkaStateId.java
+++ kafka-consumer/src/main/java/com/redhat/demo/model/KafkaStateId.java
@@ -31,4 +31,35 @@ public class KafkaStateId implements Serializable {
     public void setOffsetN(long offsetN) {
         this.offsetN = offsetN;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((topic == null) ? 0 : topic.hashCode());
+        result = prime * result + partition;
+        result = prime * result + (int) (offsetN ^ (offsetN >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        KafkaStateId other = (KafkaStateId) obj;
+        if (topic == null) {
+            if (other.topic != null)
+                return false;
+        } else if (!topic.equals(other.topic))
+            return false;
+        if (partition != other.partition)
+            return false;
+        if (offsetN != other.offsetN)
+            return false;
+        return true;
+    }
 }
