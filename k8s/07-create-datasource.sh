cac0ebede27d8897d30b3cd40d284843c9c85e8d
Donato Marrazzo <donato.marrazzo@gmail.com>
2023-06-27 16:10:47 +0200

    monitoring config improved

--- k8s/07-create-datasource.sh
+++ k8s/07-create-datasource.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-export GRAFANA_ACCESS_TOKEN=`oc create token grafana-serviceaccount --duration 10000m`
 
-echo "apiVersion: 1
-datasources:
-- name: Prometheus
-  type: prometheus
-  url: https://thanos-querier.openshift-monitoring.svc.cluster.local:9091
-  access: proxy
-  basicAuth: false
-  withCredentials: false
-  isDefault: true
-  jsonData:
-    timeInterval: 5s
-    tlsSkipVerify: true
-    httpHeaderName1: \"Authorization\"
-  secureJsonData:
-    httpHeaderValue1: \"Bearer ${GRAFANA_ACCESS_TOKEN}\"
-  editable: true" > datasource.yaml
-  
-  oc create configmap grafana-config -n my-kafka --from-file=datasource.yaml
+## Service account
+oc apply -f k8s/06-graphana-auth.yaml -n my-kafka
 
-  rm datasource.yaml
+## Token
+export GRAFANA_ACCESS_TOKEN=`oc create token grafana-serviceaccount --duration 720h`
+
+## Config map
+oc create configmap grafana-config -n my-kafka --from-literal=datasource.yaml="$(cat k8s/07-datasource.yaml.template | envsubst)"
