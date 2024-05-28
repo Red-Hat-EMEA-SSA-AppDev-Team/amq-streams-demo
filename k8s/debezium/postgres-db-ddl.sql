1111755dc784996626ad395ae36643da61235fd1
Donato Marrazzo <donato.marrazzo@gmail.com>
2024-05-28 13:21:56 +0200

    jdbc sink connnector

--- k8s/debezium/postgres-db-ddl.sql
+++ k8s/debezium/postgres-db-ddl.sql
@@ -1,11 +1,21 @@
 \connect debezium;
-CREATE TABLE customers (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+CREATE TABLE shipping (
+  order_id INT NOT NULL PRIMARY KEY,
+  shipping_method VARCHAR(50) NOT NULL,
+  shipping_address VARCHAR(200) NOT NULL
 );
-INSERT INTO customers (name, email)
-VALUES ('John Doe', 'johndoe@example.com'),
-       ('Jane Doe', 'janedoe@example.com'),
-       ('Peter Jones', 'peterjones@example.com');
+
+INSERT INTO shipping (order_id, shipping_method, shipping_address)
+VALUES (100, 'Standard', 'Main Street');
+
+CREATE TABLE payments (
+  payment_id SERIAL PRIMARY KEY,
+  order_id INT,
+  payment_method VARCHAR(50) NOT NULL,
+  transaction_id VARCHAR(50),
+  payment_amount DECIMAL(10,2) NOT NULL,
+  payment_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  payment_status VARCHAR(50) NOT NULL DEFAULT 'Pending'
+);
+
