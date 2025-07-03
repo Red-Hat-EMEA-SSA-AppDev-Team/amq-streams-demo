7005b19aa918e333243c7925727cce1623ecb6c2
Donato Marrazzo <donato.marrazzo@gmail.com>
2025-07-03 17:20:02 +0200

    grafana deployment refactoring

--- k8s/monitor/01-create-datasource.sh
+++ k8s/monitor/01-create-datasource.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Token
-export GRAFANA_ACCESS_TOKEN=`oc create token grafana-serviceaccount --duration 720h`
+export GRAFANA_ACCESS_TOKEN=`oc extract secret/secret-sa --keys=token --to=-`
 
 ## Config map
 oc create configmap grafana-config --from-literal=datasource.yaml="$(cat k8s/monitor/01-datasource.yaml.template | envsubst)"
