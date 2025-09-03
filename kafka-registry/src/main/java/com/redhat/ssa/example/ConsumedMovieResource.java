81370295fc57b3523ca371bd7c65db0144d43ad9
Donato Marrazzo <donato.marrazzo@gmail.com>
2025-09-03 10:55:28 +0200

    naming upgrade and doc improved

--- kafka-registry/src/main/java/com/redhat/ssa/example/ConsumedMovieResource.java
+++ kafka-registry/src/main/java/com/redhat/ssa/example/ConsumedMovieResource.java
@@ -13,17 +13,30 @@ import com.redhat.quarkus.kafka.Movie;
 
 import io.smallrye.mutiny.Multi;
 
+/**
+ * REST resource for consuming movies from Kafka and streaming them to clients.
+ * This resource provides a Server-Sent Events (SSE) endpoint that streams
+ * movies consumed from a Kafka topic in real-time.
+ */
 @ApplicationScoped
 @Path("/consumed-movies")
 public class ConsumedMovieResource {
     
+    /**
+     * Multi stream of movies consumed from Kafka.
+     */
     @Channel("movies-from-kafka")
     Multi<Movie> movies;
 
+    /**
+     * Streams movies consumed from Kafka to clients.
+     * @return A Multi stream of Movie objects.
+     */
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @RestStreamElementType(MediaType.TEXT_PLAIN)
     public Multi<String> stream() {
+        // Map each Movie object to its string representation for streaming to clients
         return movies.map(movie -> movie.toString());
     }
 }
--- kafka-registry/src/main/java/com/redhat/ssa/example/MovieResource.java
+++ kafka-registry/src/main/java/com/redhat/ssa/example/MovieResource.java
@@ -10,13 +10,26 @@ import org.jboss.logging.Logger;
 
 import com.redhat.quarkus.kafka.Movie;
 
+/**
+ * REST resource for enqueuing movies to Kafka.
+ * This resource provides a POST endpoint that allows clients to send
+ * Movie objects to a Kafka topic.
+ */
 @Path("/movies")
 public class MovieResource {
-        private static final Logger LOGGER = Logger.getLogger(MovieResource.class);
+    private static final Logger LOGGER = Logger.getLogger(MovieResource.class);
 
+    /**
+     * Emitter for sending movies to Kafka.
+     */
     @Channel("movies")
     Emitter<Movie> emitter;
 
+    /**
+     * Enqueues a movie to Kafka.
+     * @param movie The Movie object to enqueue.
+     * @return A Response object indicating the status of the operation.
+     */
     @POST
     public Response enqueueMovie(Movie movie) {
         LOGGER.infof("Sending movie %s to Kafka", movie.getTitle());
